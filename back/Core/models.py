ffc19af225fb38cbe2ab0c19b9a1bb21fafe215a
fat83dotcom <fernando.quimic@gmail.com>
2025-08-02 20:37:48 -0300

    📦 Criação de modelos e endpoint inicial para gerenciamento de alunos
    - Adicionados os modelos: `Student`, `Module`, `GradeReport` e `GradeReportModule` com seus respectivos relacionamentos
    - Criado `StudentViewSet` com suporte a operações CRUD utilizando Django REST Framework
    - Registrado o app `Core` e configurado o `REST_FRAMEWORK` no `settings.py`
    - Substituído o endpoint padrão da API para usar as URLs do app `Core`

--- back/Core/models.py
+++ back/Core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    address = models.CharField(max_length=255)
+    birthdate = models.DateField()
+
+    # def __str__(self):
+    #     return self.name
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    # def __str__(self):
+    #     return self.name
+
+
+class GradeReport(models.Model):
+    student = models.ForeignKey(to='Student', on_delete=models.CASCADE)
+    module = models.ManyToManyField('Module', through='GradeReportModule')
+    academic_year = models.CharField(max_length=4)
+    semester = models.CharField(choices=[
+    ('1', '1st Semester'),
+    ('2', '2nd Semester'),
+])
+
+
+class GradeReportModule(models.Model):
+    grade_report = models.ForeignKey('GradeReport', on_delete=models.CASCADE)
+    module = models.ForeignKey('Module', on_delete=models.CASCADE)
+    grade = models.FloatField()
